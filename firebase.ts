9efca2474698a85ee110f70a5cae49f0276d794d
smatikurrahman39-art <smatikurrahman39@gmail.com>
2026-04-19 02:01:55 +0600

    Remove unnecessary line in firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, User } from 'firebase/auth';
 import { getFirestore, doc, setDoc, getDoc, onSnapshot, collection, getDocs, writeBatch, getDocFromServer } from 'firebase/firestore';
 import firebaseConfig from './firebase-applet-config.json';
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
